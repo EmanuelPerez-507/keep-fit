a956282100544a1566a9783fd8ac2c7feb54bca5
Emanuel <ejpm40@gmail.com>
2023-02-06 23:33:05 +0000

    Added home screen view

---
+++ app/src/main/java/com/example/keepfit/Home/View/HomeScreen.kt
@@ -0,0 +1,11 @@
+package com.example.keepfit.Home.View
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+
+    Text("Home Screen")
+
+}
