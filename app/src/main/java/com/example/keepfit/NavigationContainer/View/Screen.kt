e625dbbd11e10056e46e67fbaad2c482097ff60d
Emanuel <ejpm40@gmail.com>
2023-02-07 01:50:00 +0000

    changed icons in navbar

---
+++ app/src/main/java/com/example/keepfit/NavigationContainer/View/Screen.kt
@@ -0,0 +1,19 @@
+package com.example.keepfit.NavigationContainer.View
+
+import com.example.keepfit.R
+
+class Screen(val label:String, val icon: Int, val route:String = "") {
+
+    companion object{
+
+        val Home = Screen("Home", R.drawable.home_icon, "Home")
+        val Goals = Screen("Goals", R.drawable.ribbon_icon, "Goals")
+        val History = Screen("History", R.drawable.history_icon, "History")
+
+        val all = listOf(
+            Home,Goals, History
+        )
+
+    }
+
+}
