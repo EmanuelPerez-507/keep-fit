0217d1bb5aeb67d02f2efbd5e2b88eb6f137ebe1
matthewmungall96 <matthewmungall96@gmail.com>
2023-02-07 15:39:19 +0000

    Update HomeScreen.kt
    Added Date to Homepage

--- app/src/main/java/com/example/keepfit/Home/View/HomeScreen.kt
+++ app/src/main/java/com/example/keepfit/Home/View/HomeScreen.kt
@@ -2,10 +2,21 @@ package com.example.keepfit.Home.View
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.util.Date
 
 @Composable
 fun HomeScreen() {
+    val current = LocalDateTime.now()
 
-    Text("Home Screen")
+    val formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)
+    val formatted = current.format(formatter)
 
+    Text("Today\n $formatted")
 }
+
+
