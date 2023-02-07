2543f800d4e788b38453de167dd868bc55181b72
Emanuel <ejpm40@gmail.com>
2023-02-07 16:44:46 +0000

    added preview annotation, added color gray, changed the nav bar color

--- app/src/main/java/com/example/keepfit/Home/View/HomeScreen.kt
+++ app/src/main/java/com/example/keepfit/Home/View/HomeScreen.kt
@@ -1,11 +1,27 @@
 package com.example.keepfit.Home.View
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
+@Preview
 fun HomeScreen() {
 
-    Text("Home Screen")
+    Box(
+        modifier = Modifier.fillMaxSize().border(1.dp, Color(0x00ff00))
+    ){
+
+        Text(modifier = Modifier.align(Alignment.Center),
+            text ="Home Screen")
+
+    }
 
 }
