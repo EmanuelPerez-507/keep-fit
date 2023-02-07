58d8ab6dd3fadd6c5f85d416a0fe405ec18e5575
Emanuel <ejpm40@gmail.com>
2023-02-07 01:33:26 +0000

    Added history screen

---
+++ app/src/main/java/com/example/keepfit/History/View/HistoryScreen.kt
@@ -0,0 +1,11 @@
+package com.example.keepfit.History.View
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HistoryScreen() {
+
+    Text("History Screen")
+
+}
