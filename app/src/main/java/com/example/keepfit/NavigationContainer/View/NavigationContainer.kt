93b43b719acab08dbd299243d36f0caa1cbf7e36
Emanuel <ejpm40@gmail.com>
2023-02-12 05:42:10 +0000

    Set default screen to goals

--- app/src/main/java/com/example/keepfit/NavigationContainer/View/NavigationContainer.kt
+++ app/src/main/java/com/example/keepfit/NavigationContainer/View/NavigationContainer.kt
@@ -90,7 +90,7 @@ fun NavigationContainer() {
                     .fillMaxSize(),
 //                    .border(3.dp, Color(0xFFFF0000)),
                 navController = navController,
-                startDestination = Screen.Home.route){
+                startDestination = Screen.Goals.route){
 
                 composable(Screen.Home.route){ HomeScreen() }
 
