439b04c3c18dd5fcaac0456ee70818fc64c350c9
Emanuel <ejpm40@gmail.com>
2023-02-13 05:51:54 +0000

    Added room compatibillity

--- app/src/main/java/com/example/keepfit/Start.kt
+++ app/src/main/java/com/example/keepfit/Start.kt
@@ -9,13 +9,23 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.Room
+import com.example.keepfit.DataLayer.Goals.Goal
+import com.example.keepfit.DataLayer.KeepFitDB
 import com.example.keepfit.NavigationContainer.NavigationContainer
 import com.example.keepfit.ui.theme.KeepFitTheme
 
 class Start : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val goalHistoryDB = Room.databaseBuilder(
+            applicationContext, KeepFitDB::class.java, "goal-history-db"
+        ).build()
+
         setContent {
             NavigationContainer()
         }
