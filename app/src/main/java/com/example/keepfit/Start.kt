87369993f039121b13aa5f425613ce06152e5457
Emanuel <ejpm40@gmail.com>
2023-02-13 06:01:42 +0000

    Added first row to the db in order to test

--- app/src/main/java/com/example/keepfit/Start.kt
+++ app/src/main/java/com/example/keepfit/Start.kt
@@ -17,14 +17,26 @@ import com.example.keepfit.DataLayer.Goals.Goal
 import com.example.keepfit.DataLayer.KeepFitDB
 import com.example.keepfit.NavigationContainer.NavigationContainer
 import com.example.keepfit.ui.theme.KeepFitTheme
+import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class Start : ComponentActivity() {
+
+    val dbThred:ExecutorService = Executors.newSingleThreadExecutor()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val goalHistoryDB = Room.databaseBuilder(
             applicationContext, KeepFitDB::class.java, "goal-history-db"
         ).build()
+
+        val newGoal = Goal(0, "Goal1", 3000, Color.Blue.toArgb())
+
+        dbThred.submit {
+            goalHistoryDB.Goals().create(newGoal)
+        }
 
         setContent {
             NavigationContainer()
